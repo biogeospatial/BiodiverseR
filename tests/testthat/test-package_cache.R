2b645a872135e91e9d3f8d013b3c62a3e2eb5a78
shawnlaffan <shawnlaffan@gmail.com>
2024-12-06 16:39:11 +1100

    Run indices tests twice to trigger cache access on second

--- tests/testthat/test-package_cache.R
+++ tests/testthat/test-package_cache.R
@@ -3,14 +3,18 @@ library("BiodiverseR")
 test_that("Test to see if package cache is created after analysis call", {
   bd = basedata$new(cellsizes=c(500,500))
 
-  result = bd$get_indices_metadata()
+  #  run twice to ensure we hit the cache
+  for (i in 1:2) {
+    result = bd$get_indices_metadata()
 
-  # Simple checks for correct type and to check if env was properly populated
-  expect_equal(typeof(result), "list")
-  expect_true(length(result) > 0)
+    # Simple checks for correct type and to check if env was properly populated
+    expect_equal(typeof(result), "list")
+    expect_true(length(result) > 0)
 
-  #print (names (result))
-  names_are_all_calcs = all (grepl (r"(^calc_)", names(result)))
+    #print (names (result))
+    names_are_all_calcs = all (grepl (r"(^calc_)", names(result)))
 
-  expect_true (names_are_all_calcs)
+    expect_true (names_are_all_calcs)
+  }
+
 })
