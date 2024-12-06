efef8077597ec2da327063cf0509f16249f8e586
shawnlaffan <shawnlaffan@gmail.com>
2024-12-06 16:17:54 +1100

    Update metadata cache tests to match preceding commit

--- tests/testthat/test-package_cache.R
+++ tests/testthat/test-package_cache.R
@@ -3,30 +3,14 @@ library("BiodiverseR")
 test_that("Test to see if package cache is created after analysis call", {
   bd = basedata$new(cellsizes=c(500,500))
 
-  expect_equal(bd$cache_list, list())
-  r_data = list (
-    data = list(
-      '250:250' = list (r1 = 13, r2 = 13, r3 = 13),
-      '250:750' = list (r2 = 11, r3 = 11, r4 = 10),
-      '750:250' = list (r3 = 10),
-      '750:750' = list (r1 =  8, r3 =  8)
-    )
-  )
-  params = list (bd_params = r_data)
-  results = bd$load_data(params)
-
-  results = bd$run_spatial_analysis (
-      calculations = c("calc_richness", "calc_endemism_central"),
-      spatial_conditions = "sp_self_only()"   
-  )
-
-  cache_type = "test"
-  result = bd$get_indices_metadata(cache_type)
+  result = bd$get_indices_metadata()
 
   # Simple checks for correct type and to check if env was properly populated
-  expect_equal(typeof(result), "environment")
+  expect_equal(typeof(result), "list")
   expect_true(length(result) > 0)
 
-  # Check if the content of the env exists
-  expect_true(!is.null(result$indices_metadata))
+  #print (names (result))
+  names_are_all_calcs = all (grepl (r"(^calc_)", names(result)))
+
+  expect_true (names_are_all_calcs)
 })
